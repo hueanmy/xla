ab439166d6a0cae57c9cb78d7d35a0f6faee99c8
hueanmy <ltmai93@gmail.com>
2017-12-11 15:30:18 +0700

    done gaussian simple

--- main_program.cpp
+++ main_program.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-using namespace std;
+#include <opencv2/opencv.hpp>
 
+using namespace std;
+using namespace cv;
+
+void gaussianBlur(Mat image) {
+  Mat blurredImage;
+  GaussianBlur( image, blurredImage, Size( 9, 9 ), 1.0);
+
+  imshow("Blurred Image" , blurredImage);
+  waitKey(0);
+}
 // we return whether the menu was processed correctly
-bool processMenu() {
+bool showMenu(Mat image) {
   // show the user the menu
   cout << "Main Menu" << endl;
   cout << "Select your favorite food" << endl;
@@ -10,30 +20,34 @@ bool processMenu() {
   cout << "2. Ramen" << endl;
   cout << "3. Mashed Potatos" << endl;
 
-  // get the response
   char choice;
   cin >> choice;
 
-  // process the response
-  // do something for each valid option
   if (choice=='1') {
-    cout << "Hold the pickle, hold the lettuce." << endl;
+    gaussianBlur(image);
   } else if (choice=='2') {
     cout << "Yum, with extra naruto." << endl;
   } else if (choice=='3') {
     cout << "Don't forget the gravy." << endl;
   } else {
-    // if we got this far, the response was not valid
     return false;
   }
   return true;
 }
 
-int main () {
-  // we loop until the menu is processed
-  while (!processMenu()) {
-    // if we get here, the menu was not processed correctly
-    // so we bother them some more
+
+int main (int argc, char** argv) {
+
+  Mat image;
+
+  image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+
+  if(!image.data){
+      cout<<"Error loading image" << "\n";
+      return -1;
+  }
+
+  while (!showMenu(image)) {
     cout << "Sorry, that is not a valid choice." << endl;
     cout << "Please try again." << endl << endl << endl;
   }
